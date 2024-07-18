1e8cab611e949cab30b7734df65bbb6777658665
DharithriSharma <123744847+DharithriSharma@users.noreply.github.com>
2024-07-18 21:18:24 +0530

    Add files via upload

---
+++ VoiceAssistantForVisuallyImpaired/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import VoiceAssistant from './VoiceAssistant';
+
+function App() {
+  return (
+    <div className="App">
+      <VoiceAssistant />
+    </div>
+  );
+}
+
+export default App;
